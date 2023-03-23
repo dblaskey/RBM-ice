2226ff3a4078061cf039891c903d7f30983c9630
Dylan Blaskey <67331267+dblaskey@users.noreply.github.com>
2023-03-23 14:29:59 -0600

    Allocate variable, comment errors

--- src/Energy_ICE.f90
+++ src/Energy_ICE.f90
@@ -15,7 +15,7 @@ real                      :: Ltnt_Heat,Sens_Heat
 real                      :: cndctvy,delta_ice,LW_back,LW_in,SW_in
 real                      :: dvsr,delta_Temp
 real                      :: T_B,T_ice,T_p,T_p_cubed,T_riv,T_srfc
-real                      :: snow_p,cndctvySI,snow_cndctvy,thickS,T_S
+real                      :: cndctvySI,snow_cndctvy,thickS,T_S
 real                      :: delta_snow,lvfs 
 !
 real,parameter            :: thick0 = 0.8
@@ -38,24 +38,24 @@ real,parameter            :: thick0 = 0.8
   Sens_Heat = rho_air*cp_air*Ch_Cg*wind(ncell)*(dbt(ncell)-T_srfc)
 !
 
-if (snow_p .gt. 0.01) then ! Need to allocate snow_p
+if (snow_thick(nr,ns,n1) .gt. 0.01) then 
   SW_in = (1.0-snow_albedo)*QNS(ncell)
-  cndctvySI = ice_cndctvy*snow_cndctvy/(thick0*snow_cndctvy)               & ! Allocated cndctvySI, snow_cndctvy, thickS
+  cndctvySI = ice_cndctvy*snow_cndctvy/(thick0*snow_cndctvy)               & 
               + thickS*ice_cndctvy
   dvsr = 4.0*epsilon*Stfn_Bltz*T_p_cubed + cndctvySI 
   delta_Temp = (Sens_Heat + Ltnt_Heat + LW_in + SW_in - LW_back           &
-            +cndctvySI*(T_B-T_srfc))/dvsr
+            +cndctvySI*(T_B-T_srfc))/dvsr !THIS IS NOT RIGHT, need to change snow temperature not ice temp
 !  
-  snow_temp(nr,ns,n2) = snow_temp(nr,ns,n1) + delta_Temp !ALLOCATE SNOW TEMP
+  snow_temp(nr,ns,n2) = snow_temp(nr,ns,n1) + delta_Temp
   snow_temp(nr,ns,n2) = AMAX1(-.01,snow_temp(nr,ns,n2))
   ! Check here to see if snow is thawing 
   !
-  if (snow_temp(nr,ns,n2) .ge. T_S) then !ALLOCATE T_S
+  if (snow_temp(nr,ns,n2) .ge. T_S) then 
     ! NEED TO DOUBLE CHECK THIS IS RIGHT
-    delta_snow = dt_comp*(Sens_Heat + Ltnt_Heat + LW_in + SW_in - LW_back)/lvfs ! ALLOCATE DELTA SNOW, LVFS
+    delta_snow = dt_comp*(Sens_Heat + Ltnt_Heat + LW_in + SW_in - LW_back)/lvfs
     snow_thick(nr,ns,n2) = snow_thick(nr,ns,n1) - delta_snow                
     snow_thick(nr,ns,n2) = AMAX1(snow_thick(nr,ns,n2),0.0)
-  else ! SHOULD AT LEAST UPDATE TO ACCOUNT FOR INSULATION OF ICE (changed to conductivity of snow and ice)
+  else !THIS DOESN'T LOOK RIGHT EITHER
     T_ice = ice_temp(nr,ns,n1)
     T_riv = temp(nr,ns,n1)
     delta_ice = dt_comp*cndctvySI*(T_riv-T_ice)/lvf
@@ -64,23 +64,21 @@ if (snow_p .gt. 0.01) then ! Need to allocate snow_p
     ICE(ncell) = 200.
   end if
 else
-  SW_in = (1.0-0.4*I0)*(1.0-ice_albedo)*QNS(ncell) ! Check if this is NEEDED in Vapor, may need to move all of Vapor
+  SW_in = (1.0-0.4*I0)*(1.0-ice_albedo)*QNS(ncell)
   
   delta_Temp = (Sens_Heat + Ltnt_Heat + LW_in + SW_in - LW_back           &
             +cndctvy*(T_B-T_srfc))/dvsr
   cnd_tmp = cndctvy*(T_B-T_srfc)
-
-
+!
   ice_temp(nr,ns,n2) = ice_temp(nr,ns,n1) + delta_Temp
   ice_temp(nr,ns,n2) = AMAX1(-.01,ice_temp(nr,ns,n2))
 !
-!
-! Check here to see if ice is thawing UPDATE TO MOVE THIS INSIDE IF ELSE SNOW STATEMENT
+! Check here to see if ice is thawing
 !
   if (ice_temp(nr,ns,n2) .ge. T_B) then
     
     delta_ice = dt_comp*(Sens_Heat + Ltnt_Heat + LW_in + SW_in - LW_back)/lvf
-   ice_thick(nr,ns,n2) = ice_thick(nr,ns,n1) - delta_ice                
+    ice_thick(nr,ns,n2) = ice_thick(nr,ns,n1) - delta_ice                
     ice_thick(nr,ns,n2) = AMAX1(ice_thick(nr,ns,n2),0.0)
     ice_temp(nr,ns,n2) = AMIN1(ice_temp(nr,ns,n2),T_B)
     ice_temp(nr,1:ns,n2) = T_B
@@ -104,17 +102,8 @@ end if
       ICE(ncell) = 101.
       ice_thick(nr,ns,n2) = 0.00
     end if
-!
-else
-  
-!
-! If ice thickness is less than the minimum, river is no longer frozen
-!
-    if (ice_thick(nr,ns,n2) .lt. 0.01) then 
-      ICE(ncell) = 101.
-      ice_thick(nr,ns,n2) = 0.00
-    end if
-!
+! 
+! If ice thickness is greater than river depth, set to river depth
 !
   if (ice_thick(nr,ns,n2) .gt. depth(ncell)) ice_thick(nr,ns,n2) = depth(ncell)
 !
